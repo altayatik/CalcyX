2482d58c763965fc71d3cff1f0a733f8077442c9
Altay Atik <altayatik@Altays-MacBook-Air.local>
2018-09-04 21:31:08 +0300

    Initial commit

---
+++ app/src/main/java/calcyapps/calcyx/Preferences.java
@@ -0,0 +1,63 @@
+package calcyapps.calcyx;
+
+
+import android.annotation.TargetApi;
+import android.app.ActionBar;
+import android.os.Build;
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+import android.support.v4.app.NavUtils;
+import android.view.MenuItem;
+
+
+public class Preferences extends PreferenceActivity {
+
+	private static final boolean ALWAYS_SIMPLE_PREFS = false;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setupActionBar();
+		
+	}
+
+
+	@TargetApi(Build.VERSION_CODES.HONEYCOMB)
+	private void setupActionBar() {
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+			// Show the Up button in the action bar.
+			ActionBar actionbar = getActionBar();
+			actionbar.setDisplayHomeAsUpEnabled(true);
+		}
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			// This ID represents the Home or Up button. In the case of this
+			// activity, the Up button is shown. Use NavUtils to allow users
+			// to navigate up one level in the application structure. For
+			// more details, see the Navigation pattern on Android Design:
+			//
+			// http://developer.android.com/design/patterns/navigation.html#up-vs-back
+			//
+			// TODO: If Settings has multiple levels, Up should navigate up
+			// that hierarchy.
+			NavUtils.navigateUpFromSameTask(this);
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	@Override
+	protected void onPostCreate(Bundle savedInstanceState) {
+		super.onPostCreate(savedInstanceState);
+		addPreferencesFromResource(R.xml.preferences);
+
+	}
+	
+
+
+
+}
